fa3c86fec89db375ea4ef551f36bce54d5e6d67b
Ugonna Okoli <ugonna.okoli@boku.ltd>
2018-12-15 13:59:07 +0100

    Initial

---
+++ src/Card.kt
@@ -0,0 +1,8 @@
+val clubs = "Clubs"
+val diamonds = "Diamonds"
+val hearts = "Hearts"
+val spades = "Spades"
+val redSuits = arrayOf(diamonds, hearts)
+val blackSuits = arrayOf(clubs, spades)
+
+data class Card (val value: Int, val suit: String, var faceUp: Boolean = false)
---
+++ src/Deck.kt
@@ -0,0 +1,20 @@
+import java.util.*
+
+class Deck {
+    val cards = Array(52, { Card(it % 13, getSuit(it)) })
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset(){
+        cardsInDeck = cards.toMutableList()
+        Collections.shuffle(cardsInDeck)
+    }
+
+    private fun getSuit(i: Int) = when (i / 13) {
+            0 -> clubs
+            1 -> diamonds
+            2 -> hearts
+            else -> spades
+        }
+}
